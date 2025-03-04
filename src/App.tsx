9e4291a87b95d70de5034f627342d9f09868d138
wizzzx <alexkryukovzzx@gmail.com>
2025-03-04 17:45:51 +0300

    set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import styles from "./App.module.scss";
+import "../public/styles/main.scss";
+
 function App() {
   return (
     <>
+      <header className={styles.header_container}>
+        <div className={styles.header_logo}>CV Generator</div>
+        <img src="" alt="github logo" className={styles.github_img} />
+      </header>
       <div></div>
     </>
   );
